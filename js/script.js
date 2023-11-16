00b2e244153f4079000f19223ccb0d2ed649b284
Saad <saadxdhillon@gmail.com>
2023-11-17 01:24:48 +0500

    updating changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 
-
 function isElementInView(el) {
     const rect = el.getBoundingClientRect();
+    const threshold = 1;
+
     return (
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        rect.bottom >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) * threshold
     );
 }
+
 
 function showElementsOnScroll() {
     const lines = document.querySelectorAll('.line');
@@ -27,30 +29,45 @@ function showElementsOnScroll() {
             icon.style.opacity = 0;
         }
     })
-    fadingElements.forEach((fadeEl) =>{
-        if(isElementInView(fadeEl)){
+    fadingElements.forEach((fadeEl) => {
+        if (isElementInView(fadeEl)) {
             fadeEl.style.opacity = 1;
-        }else{
+        } else {
             fadeEl.style.opacity = 0;
         }
     })
 }
 
-document.addEventListener('DOMContentLoaded',showElementsOnScroll);
+document.addEventListener('DOMContentLoaded', showElementsOnScroll);
 window.addEventListener('scroll', showElementsOnScroll);
 
 
 
 // following green blured bubble
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const bubble = document.querySelector(".green-bubble");
 
-    document.addEventListener("mousemove", function(e) {
+    document.addEventListener("mousemove", function (e) {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
 
         bubble.style.left = (mouseX - 300) + "px";
         bubble.style.top = (mouseY - 230) + "px";
+    });
+});
+
+
+// scroll
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - ($(window).height() - target.outerHeight()) / 2
+            }, 1000);
+        }
     });
 });
